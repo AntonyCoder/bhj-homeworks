f95e97bb99345f31d8b68a29a04e626b01dd47f8
Anton Lysenko <antonlysenko161@yandex.ru>
2024-05-03 12:56:59 +0300

    Исправление ДЗ

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -5,12 +5,10 @@ document.addEventListener('scroll', () => {
     block.forEach(e => {
         const viewportHeight = window.innerHeight;
         const elementTop = e.getBoundingClientRect().top;
-        const elementBottom = e.getBoundingClientRect().bottom;
 
-        if (elementTop > 0 || elementBottom < viewportHeight) {
+        if (elementTop > 0 && elementTop < viewportHeight) {
             e.classList.add('reveal_active');
-        }
-        if (elementTop < 0 || elementBottom > viewportHeight) {
+        } else {
             e.classList.remove('reveal_active');
         }
     })
